21a2438714028d7c73fe3a9f67a9081758125d91
k_ito <k_ito@asobimo.com>
2019-12-19 19:37:49 +0900

    Core

--- Template/Assets/Core/UGUI/Scripts/UIUtil.cs
+++ Template/Assets/Core/UGUI/Scripts/UIUtil.cs
@@ -23,5 +23,91 @@ namespace GameSystem.UI
 			}
 			return ui.GetComponent<T>();
 		}
+
+		private static CanvasGroup GetGroup(this GameObject obj)
+		{
+			return obj.GetOrAddComponent<CanvasGroup>();
+		}
+
+		public static void Enable(GameObject obj, bool enable)
+		{
+			var group = obj.GetGroup();
+			group.alpha = enable ? 1.0f : 0.5f;
+			group.blocksRaycasts = enable;
+		}
+
+		/// <summary>
+		/// 表示切替
+		/// </summary>
+		public static void Visible(GameObject obj, bool visible)
+		{
+			var group = obj.GetGroup();
+			group.alpha = visible ? 1.0f : 0.0f;
+			group.blocksRaycasts = visible;
+		}
+
+		/// <summary>
+		/// 隠す
+		/// </summary>
+		public static IEnumerator Hide(System.Action complete, GameObject obj, float duration)
+		{
+			var group = obj.GetGroup();
+
+			yield return Fade(false, obj, duration);
+			group.blocksRaycasts = false;
+
+			complete?.Invoke();
+		}
+
+		/// <summary>
+		/// 見せる
+		/// </summary>
+		public static IEnumerator Display(System.Action complete, GameObject obj, float duration)
+		{
+			var group = obj.GetGroup();
+
+			group.blocksRaycasts = false;
+			yield return Fade(true, obj, duration);
+			group.blocksRaycasts = true;
+
+			complete?.Invoke();
+		}
+
+		/// <summary>
+		/// 止める
+		/// </summary>
+		public static IEnumerator Stop(System.Action complete, GameObject obj, float duration)
+		{
+			yield return Hide(complete, obj, duration);
+			obj.SetActive(false);
+		}
+
+		/// <summary>
+		/// 起動
+		/// </summary>
+		public static IEnumerator Awake(System.Action complete, GameObject obj, float duration)
+		{
+			obj.SetActive(true);
+			yield return Display(complete, obj, duration);
+		}
+
+		/// <summary>
+		/// フェード
+		/// </summary>
+		public static IEnumerator Fade(bool fadeIn, GameObject obj, float fadeSec)
+		{
+			uTools.TweenAlpha tween;
+
+			if(fadeIn)
+			{
+				tween = uTools.TweenAlpha.Begin(obj, 0.0f, 1.0f, fadeSec);
+			}
+			else
+			{
+				tween = uTools.TweenAlpha.Begin(obj, 1.0f, 0.0f, fadeSec);
+			}
+
+			yield return new WaitWhile(() => tween && tween.enabled);
+		}
 	}
 }
