75b5035c77e3cff1c99fb3c1cddc89d59d033699
k_ito <k_ito@asobimo.com>
2019-12-19 19:51:02 +0900

    Core

---
+++ Template/Assets/Core/UGUI/Scripts/Common/SimpleFade.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GameSystem.UI
+{
+	public class SimpleFade : ManagedPanel
+	{
+		private static SimpleFade _instance;
+
+		public override ManagedPanel CreatePanel(Transform parent)
+		{
+			gameObject.AddComponent<CanvasGroup>();
+
+			var image = gameObject.AddComponent<UnityEngine.UI.Image>();
+			image.color = Color.black;
+			image.rectTransform.anchorMin = new Vector2(0, 0);
+			image.rectTransform.anchorMax = new Vector2(1, 1);
+			image.rectTransform.offsetMin = new Vector2(0, 0);
+			image.rectTransform.offsetMax = new Vector2(1, 1);
+			image.raycastTarget = false;
+
+			transform.SetParent(parent, false);
+			return this;
+		}
+
+		private static SimpleFade Create()
+		{
+			var component = new GameObject("FadeView").AddComponent<SimpleFade>();
+			UIManager.Instance.Register(component, UIManager.Layer.Overlay);
+			_instance = component;
+			return component;
+		}
+
+		private static Coroutine Begin(float duration, bool fadeIn)
+		{
+			if(_instance == null)
+			{
+				Create();
+			}
+			if(fadeIn)
+			{
+				UIUtil.Visible(_instance.gameObject, false);
+				return _instance.StartCoroutine(UIUtil.Display(null, _instance.gameObject, duration));
+			}
+			else
+			{
+				UIUtil.Visible(_instance.gameObject, true);
+				return _instance.StartCoroutine(UIUtil.Hide(null, _instance.gameObject, duration));
+			}
+		}
+
+		public static Coroutine FadeIn(float duration = 0.5f)
+		{
+			return Begin(duration, true);
+		}
+
+		public static Coroutine FadeOut(float duration = 0.5f)
+		{
+			return Begin(duration, false);
+		}
+
+		public override void OnDiscard()
+		{
+			Destroy(_instance.gameObject);
+			_instance = null;
+		}
+	}
+}
---
+++ Template/Assets/Core/UGUI/Scripts/Common/TouchPanel.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace GameSystem.UI
+{
+	public class TouchPanel : MonoBehaviour, IBeginDragHandler, IEndDragHandler, IDragHandler, IPointerClickHandler
+	{
+		private readonly float DoubleClickTime = 0.2f;
+		private readonly float FlickDistance = 8.0f * 8.0f;
+		float _prevClickTime;
+		Vector2 _dragValue;
+		int _firstId = -1;
+		int _secondId = -1;
+
+		public System.Action<Vector2> onDrag;
+		public System.Action onClick;
+		public System.Action onDoubleClick;
+		public System.Action<float> onPinch;
+		public System.Action<Vector2> onFlick;
+		
+		public void OnPointerClick(PointerEventData eventData)
+		{
+			if(_firstId != -1 || _secondId != -1) return;
+			if(_prevClickTime + DoubleClickTime > Time.realtimeSinceStartup)
+			{
+				_prevClickTime = 0.0f;
+
+				if(onDoubleClick != null)
+					onDoubleClick.Invoke();
+				return;
+			}
+			_prevClickTime = Time.realtimeSinceStartup;
+			if(onClick != null)
+			{
+				onClick.Invoke();
+			}
+		}
+
+		public void OnDrag(PointerEventData eventData)
+		{
+			// ピンチ処理
+			if(_firstId != -1 && _secondId != -1)
+			{
+				if(onPinch != null)
+					onPinch.Invoke(eventData.delta.magnitude);
+				return;
+			}
+
+			_dragValue += eventData.delta;
+			if(onDrag != null)
+				onDrag.Invoke(eventData.delta);
+		}
+
+		public void OnBeginDrag(PointerEventData eventData)
+		{
+			if(_firstId == -1)
+			{
+				_firstId = eventData.pointerId;
+				if(eventData.pointerId == -1) _firstId = 1;
+			}
+			else if(_secondId == -1)
+			{
+				_secondId = eventData.pointerId;
+				if(eventData.pointerId == -1) _secondId = 2;
+			}
+			_dragValue += eventData.delta;
+		}
+
+		public void OnEndDrag(PointerEventData eventData)
+		{
+			_firstId = -1;
+			_secondId = -1;
+			_dragValue = Vector2.zero;
+
+			if(eventData.delta.sqrMagnitude >= FlickDistance)
+			{
+				if(onFlick != null)
+					onFlick.Invoke(eventData.delta.normalized);
+			}
+			if(onDrag != null)
+				onDrag.Invoke(_dragValue);
+		}
+	}
+}
